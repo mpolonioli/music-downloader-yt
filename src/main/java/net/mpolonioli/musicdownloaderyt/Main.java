3ef618fa1d924008fe647d290cd4a49e2c3ed5fa
mpolonioli <m.polonioli@campus.unimib.it>
2018-03-19 02:10:51 +0100

    cambiato metodo di ottenimento parametri

--- src/main/java/net/mpolonioli/musicdownloaderyt/Main.java
+++ src/main/java/net/mpolonioli/musicdownloaderyt/Main.java
@@ -2,38 +2,76 @@ package net.mpolonioli.musicdownloaderyt;
 
 import java.io.File;
 import java.io.IOException;
-import java.util.HashMap;
+
+import org.apache.commons.cli.CommandLine;
+import org.apache.commons.cli.CommandLineParser;
+import org.apache.commons.cli.DefaultParser;
+import org.apache.commons.cli.HelpFormatter;
+import org.apache.commons.cli.Option;
+import org.apache.commons.cli.Options;
+import org.apache.commons.cli.ParseException;
 
 public class Main {
 
 	public static void main(String[] args) {
 		/*
-		 * 	Get the required parameters from configuration file.
+		 * 	Get the required parameters from args:
+		 *  outputDirectory, inputFile, youtubedlPath.
 		 */
-		GetProperties propertiesGetter = new GetProperties();
-		HashMap<String, String> propHashMap = null;
-		try {
-			propHashMap = propertiesGetter.getPropValues();
-		} catch (IOException e) {
-			System.err.println("There was an IO error: " + e.getCause() + " : " + e.getMessage());
-		}
+		
+		Options options = new Options();
+		
+		Option outDirPath = new Option("o", "output", true, "Output directory (default is './')");
+		options.addOption(outDirPath);
+		
+		Option inFilePath = new Option("p", "playlist", true, "Playlist file (required)");
+		inFilePath.setRequired(true);
+		options.addOption(inFilePath);
+		
+		Option dlPath = new Option("y", "youtube-dl", true, "Path to youtube-dl (default is 'youtube-dl')");
+		options.addOption(dlPath);
+		
+        CommandLineParser parser = new DefaultParser();
+        HelpFormatter formatter = new HelpFormatter();
+        CommandLine cmd;
+		
+        try {
+            cmd = parser.parse(options, args);
+        } catch (ParseException e) {
+            System.out.println(e.getMessage());
+            formatter.printHelp("music-downloader-yt", options);
 
-		File outputDirectory = null;
-		String output = propHashMap.get("outputDirectory");
-		if(output != null && !output.equals(""))
-		{
-			outputDirectory = new File(output);
-		} else 
-		{
-			outputDirectory = new File("./");
-		}
-		File inputFile = new File(propHashMap.get("inputFile"));
-		String youtubedlPath = propHashMap.get("youtubedlPath");
+            System.exit(1);
+            return;
+        }
+        
+        File inFile = new File(cmd.getOptionValue('p'));
+        
+        File outDir;
+        if(cmd.getOptionValue('o') != null) {
+        	outDir = new File(cmd.getOptionValue('o'));
+        } else {
+        	outDir = new File("./");
+        }
+        
+        String youtubeDlPath;
+        if(cmd.getOptionValue('y') != null) {
+        	youtubeDlPath = cmd.getOptionValue('y');
+        } else {
+        	youtubeDlPath = "youtube-dl";
+        }
 
-		MusicDownloader downloader = new MusicDownloader(youtubedlPath);
+        System.out.println(
+        		"Launch Parameters" + "\n" +
+        		"Output Directory: " + outDir.getAbsolutePath() + "\n" +
+        		"Playlist File: " + inFile.getAbsolutePath() + "\n" +
+        		"Youtube-dl path: " + youtubeDlPath
+        		);
+
+		MusicDownloader downloader = new MusicDownloader(youtubeDlPath);
 		
 		try {
-			downloader.downloadPlaylist(downloader.updatePlaylist(inputFile), outputDirectory);
+			downloader.downloadPlaylist(downloader.updatePlaylist(inFile), outDir);
 		} catch (IOException e) {
 			System.err.println("There was a IOException: " + e.getCause() + " : " + e.getMessage());
 		}
